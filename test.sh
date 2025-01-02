5c1c330f7e2b31859e6bc83f363ac984f0ad0012
kimp <ahmadmiqdad27@gmail.com>
2025-01-02 11:43:16 +0700

    Check the battery status

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 for bat in /sys/class/power_supply/BAT?; do
-  [ -d "$bat" ] && echo "${bat##*/}: $(cat "$bat/capacity")%"
+  if [ -d "$bat" ]; then
+    capacity=$(cat "$bat/capacity")
+    status=$(cat "$bat/status")
+    echo "${bat##*/}: $capacity% ($status)"
+  fi
 done
+
